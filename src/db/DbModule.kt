d5ed680328155bb3f4713c8351fae16f115de1ba
Vincent <VincentWilliams97@gmail.com>
2019-11-03 18:01:22 -0700

    Fix mongo error
    - Fix error when adding to db
    - Cleanup db module
    - Correctly configure db based on environment variables

--- src/db/DbModule.kt
+++ src/db/DbModule.kt
@@ -1,38 +1,42 @@
 package com.vincent.javatime.db
 
 import com.mongodb.MongoClient
+import com.mongodb.MongoClientOptions
 import com.mongodb.MongoClientURI
-import com.mongodb.MongoURI
+import com.mongodb.client.MongoCollection
 import com.mongodb.client.MongoDatabase
+
 import com.vincent.javatime.models.Fact
 import com.vincent.javatime.models.Suggestion
-import org.koin.core.qualifier.named
+
 import org.koin.dsl.module
 import org.litote.kmongo.*
 
+private const val FACTS_COLLECTION = "facts"
+private const val SUGGESTIONS_COLLECTION = "suggestions"
+
 val dbModule = module {
 
-    val databaseName= "heroku_cb6tk21z"
-    val factCollection = "facts"
-    val suggestionCollection = "suggestions"
-
-    single(named("suggestions")) {
-        get<MongoDatabase>().getCollection<Suggestion>(suggestionCollection)
+    single<MongoCollection<Suggestion>> {
+        get<MongoDatabase>().getCollection<Suggestion>(SUGGESTIONS_COLLECTION)
     }
 
-    single(named("facts")) {
-        get<MongoDatabase>().getCollection<Fact>(factCollection)
+    single<MongoCollection<Fact>> {
+        get<MongoDatabase>().getCollection<Fact>(FACTS_COLLECTION)
     }
 
-    single {
+    single<MongoDatabase> {
+        val databaseName = get<MongoClientURI>().database ?: ""
         get<MongoClient>().getDatabase(databaseName)
     }
 
-    single {
+    single<MongoClient> {
         KMongo.createClient(get<MongoClientURI>())
     }
 
-    single {
-        MongoClientURI(System.getenv("MONGODB_URI"))
+    single<MongoClientURI> {
+        val optionsBuilder = MongoClientOptions.builder()
+            .retryWrites(false)
+        MongoClientURI(System.getenv("MONGODB_URI"), optionsBuilder)
     }
 }
